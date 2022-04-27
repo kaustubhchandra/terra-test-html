a7da00101186f3ae823b42e287dbcd193a7e1a90
root <root@ip-172-31-14-193.ap-south-1.compute.internal>
2022-04-27 18:29:55 +0000

    kk

---
+++ instance.tf
@@ -0,0 +1,100 @@
+resource "aws_security_group" "kk-ssh-allowed" {
+    vpc_id = "${aws_vpc.Main.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        // This means, all ip address are allowed to ssh !
+        // Do not do it in the production.
+        // Put your office or home address in it!
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    //If you do not add this rule, you can not reach the NGIX
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#bashchain host
+
+resource "aws_instance" "bashchain-1" {
+  instance_type = "t2.micro"
+ # vpc_security_group_ids = "${aws_security_group.kk-ssh-allowed.id}" 
+  associate_public_ip_address = true
+  user_data = "${file("user-data.txt")}"
+  key_name = "kk-project"
+  ami = "ami-0851b76e8b1bce90b"
+  subnet_id = "${aws_subnet.publicsubnets.id}"
+  vpc_security_group_ids = [
+  	aws_security_group.kk-ssh-allowed.id
+  	]
+
+  tags = {
+       Name = "bashchain-1"
+ } 
+}
+
+resource "aws_instance" "bashchain-2" {
+  instance_type = "t2.micro"
+ # vpc_security_group_ids = "${aws_security_group.kk-ssh-allowed.id}"
+  associate_public_ip_address = true
+  user_data = "${file("user-data.txt")}"
+  key_name = "kk-project"
+  ami = "ami-0851b76e8b1bce90b"
+  subnet_id = "${aws_subnet.publicsubnets.id}"
+  vpc_security_group_ids = [
+        aws_security_group.kk-ssh-allowed.id
+        ]
+
+  tags = {
+       Name = "bashchain-2"
+ }
+}
+
+
+#private host
+
+resource "aws_instance" "web-1" {
+  instance_type = "t2.micro"
+ # vpc_security_group_ids = "${aws_security_group.kk-ssh-allowed.id}"
+  associate_public_ip_address = false
+  user_data = "${file("user-data.txt")}"
+  key_name = "kk-project"
+  ami = "ami-0851b76e8b1bce90b"
+  subnet_id = "${aws_subnet.privatesubnets.id}"
+  vpc_security_group_ids = [
+        aws_security_group.kk-ssh-allowed.id
+        ]
+
+  tags = {
+       Name = "web-1"
+ }
+}
+
+
+resource "aws_instance" "web-2" {
+  instance_type = "t2.micro"
+ # vpc_security_group_ids = "${aws_security_group.kk-ssh-allowed.id}"
+  associate_public_ip_address = false
+  user_data = "${file("user-data.txt")}"
+  key_name = "kk-project"
+  ami = "ami-0851b76e8b1bce90b"
+  subnet_id = "${aws_subnet.privatesubnets-2.id}"
+  vpc_security_group_ids = [
+        aws_security_group.kk-ssh-allowed.id
+        ]
+
+  tags = {
+       Name = "web-2"
+ }
+}
